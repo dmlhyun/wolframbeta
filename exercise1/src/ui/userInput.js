cc977d552a9e9ad487b76341ec178af158dc0b71
Andrew Chung <euihyun.ac@gmail.com>
2018-02-02 18:28:29 -0500

    add validator for empty expression, create utilities file

--- exercise1/src/ui/userInput.js
+++ exercise1/src/ui/userInput.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
-import { Container, Form, Input } from 'semantic-ui-react';
+import { Container, Form, Input, Label } from 'semantic-ui-react';
+import { simplifyExpression } from '../common/utilities';
 
 class UserInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expression: ''
+      expression: '',
+      result: null,
+      error: false
     };
   }
-  handleSubmit(e, val) {
-      alert(this.state.expression);
+  handleSubmit() {
+      if (!this.state.expression) {
+        this.setState({
+          error: true
+        });
+      }
+      simplifyExpression();
       // take expression and validate
       // -> if true set simplified result
       // -> if false send error
   }
   handleChange(e, data) {
     e.preventDefault();
-    this.setState({ expression: data.value })
+    this.setState({
+      expression: data.value,
+      error: false
+    })
   }
   render() {
+    console.log(this.state)
     return (
       <Container>
-        <Form onSubmit={(e, value) => this.handleSubmit(e, value)}>
+        <Form onSubmit={() => this.handleSubmit()}>
           <Input
             fluid
             value={this.state.expression}
             onChange={(e, data) => this.handleChange(e, data)}
           />
+          {this.state.error &&
+            <Label pointing color='red'>Invalid expression</Label>
+          }
         </Form>
       </Container>
     );
