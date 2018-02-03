df07ca65164dfd70bbe4043231858886564829ff
Andrew Chung <euihyun.ac@gmail.com>
2018-02-02 19:06:58 -0500

    add validation regex

--- exercise1/src/common/utilities.js
+++ exercise1/src/common/utilities.js
@@ -1,2 +1,8 @@
 export const simplifyExpression = () => {
 };
+
+export const validateExpression = (str) => {
+	const regex = /[^A-Ca-c0-1()^+.~]/g; // Checks if str has any char besides these
+	const res = regex.test(str);
+	return res;
+};
